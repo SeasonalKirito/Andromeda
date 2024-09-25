be67541e8bc97be5374a25c8aad82d7ca429c2ba
Seasonal <99521865+SeasonalKirito@users.noreply.github.com>
2024-09-24 18:21:02 -0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,8 @@
+repeat
+    task.wait()
+until game:IsLoaded()
+
+local placeID = game.PlaceId
+local github = "https://raw.githubusercontent.com/SeasonalKirito/Andromeda/main/"
+
+if placeID == "6516141723" then
