4f13b51201a6d00176da8dff9dbe04e761f2b41a
Seasonal <99521865+SeasonalKirito@users.noreply.github.com>
2024-09-24 18:22:41 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -5,4 +5,10 @@ until game:IsLoaded()
 local placeID = game.PlaceId
 local github = "https://raw.githubusercontent.com/SeasonalKirito/Andromeda/main/"
 
-if placeID == "6516141723" then
+if placeID == 6516141723 then
+    loadstring(game:HttpGet(github .. "places/6516141723.lua"))()
+elseif placeID == 1 or placeID == 2 then
+    loadstring(game:HttpGet(github .. "places/"..tostring(placeID)..".lua"))()
+else
+    print("Place not supported.")
+end
