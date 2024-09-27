ed484e3579731908447b4f53a807d5dc5ce46c9d
Seasonal <99521865+SeasonalKirito@users.noreply.github.com>
2024-09-26 17:52:16 -0700

    n

--- places/606849621.lua
+++
@@ -1,7 +0,0 @@
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/SeasonalKirito/Andromeda/main/Elerium/source.lua"))()
-local window = library:AddWindow("Andromeda | "..game.PlaceId)
-
-local tab = window:AddTab("tab")
-local console = tab:AddConsole("Console")
-tab:Show()
-library:FormatWindows()
