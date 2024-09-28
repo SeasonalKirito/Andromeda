5fa6a8ac10aef19c4a887519b8216d29d5b86059
Seasonal <99521865+SeasonalKirito@users.noreply.github.com>
2024-09-27 20:14:54 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,13 +2,14 @@ repeat
     task.wait()
 until game:IsLoaded()
 
+local placeId = game.PlaceId
 local github = "https://raw.githubusercontent.com/SeasonalKirito/Andromeda/main/"
 local supported = github.."supported.lua"
 local games = loadstring(game:HttpGet(supported))()
 
 for _, v in pairs(games) do
-    if v == game.PlaceId then
-        loadstring(game:HttpGet(github.."games/"..game.PlaceId..".lua", true))()
+    if v == placeId then
+        loadstring(game:HttpGet(github.."games/"..placeId..".lua", true))()
         break
     else
         loadstring(game:HttpGet(github.."universal.lua", true))()
