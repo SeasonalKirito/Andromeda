5689e9b76457345e0586aac82ae1c7220771ab19
Seasonal <99521865+SeasonalKirito@users.noreply.github.com>
2024-09-29 14:05:06 -0700

    Update 12552538292.lua

--- places/12552538292.lua
+++ places/12552538292.lua
@@ -59,29 +59,23 @@ local god_mode = main:Checkbox({
 
 --<< Functions >>--
 function Script.Functions.GodMode(Value: boolean)
-	if Value then
-		for _, v in pairs(workspace:WaitForChild("Rooms"):GetDescendants()) do
-			if v.Name == "Locker" and v:IsA("Model") then
-				local original_cframe = lplr.Character.HumanoidRootPart.CFrame
-				lplr.Character.HumanoidRootPart.CFrame = v:FindFirstChild("Base").CFrame
+	for _, v in pairs(workspace.Rooms:GetDescendants()) do
+		if v.Name == "Locker" and v:IsA("Model") and v.Parent:IsA("Model") then
+			local original_cframe = lplr.Character.HumanoidRootPart.CFrame
+			lplr.Character.HumanoidRootPart.CFrame = v.Root.CFrame
+			task.wait(0.5)
+			if Value then
 				v:FindFirstChild("Folder"):FindFirstChild("Enter"):InvokeServer()
-				lplr.Character.HumanoidRootPart.CFrame = original_cframe
-				break
-			end
-		end
-		--Script.Functions.Log(console, "SUCCESS", "Enabled Godmode", "rgb(0, 204, 102)", true)
-	else
-		for _, v in pairs(workspace:WaitForChild("Rooms"):GetDescendants()) do
-			if v.Name == "Locker" and v:IsA("Model") then
-				local original_cframe = lplr.Character.HumanoidRootPart.CFrame
-				lplr.Character.HumanoidRootPart.CFrame = v:FindFirstChild("Base").CFrame
+			else
 				v:FindFirstChild("Folder"):FindFirstChild("Exit"):InvokeServer()
-				lplr.Character.HumanoidRootPart.CFrame = original_cframe
-				break
 			end
+			task.wait(1)
+			lplr.Character.HumanoidRootPart.CFrame = original_cframe
+			break
 		end
-		--Script.Functions.Log(console, "SUCCESS", "Disabled Godmode", "rgb(0, 204, 102)", true)
 	end
+	--Script.Functions.Log(console, "SUCCESS", "Enabled Godmode", "rgb(0, 204, 102)", true)
+	--Script.Functions.Log(console, "SUCCESS", "Disabled Godmode", "rgb(0, 204, 102)", true)
 end
 
 function Script.Functions.Log(object: any, title: string, description: string, color: string, full: boolean)
